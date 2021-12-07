ad8cf9b4f3cf8a3c2b7d261b55515259a6648d0c
Travis CI User <travis@example.org>
2021-12-07 15:01:06 +0000

    Adding repo to travis.yml

---
+++ js/721.2ea9174d565ff765b7c1.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksandbox=self.webpackChunksandbox||[]).push([[721],{6288:(e,n,a)=>{a.r(n),a.d(n,{default:()=>l});var s=a(5697),p=a.n(s),o=a(3264),r=a.n(o),c=function(e){return r().createElement("span",{className:"sample-component"}," ",e.children," ")};c.displayName="SampleComponent",c.propTypes={children:p().oneOfType([p().arrayOf(p().node),p().node])};const l=c}}]);
+//# sourceMappingURL=../sourcemaps/721.3dd4d44c883149f3cef79fc6fb072b8c.js.map
